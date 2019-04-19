c66bab47660724122857883bd835302fd8883c48
Brian Lai <brianlai@MacBook-Pro.local>
2019-04-19 15:04:38 -0700

    Basic setup of application

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
+
+// Sets app.js to use pug as layout engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
 app.use('/', routes);
 
 module.exports = app;
